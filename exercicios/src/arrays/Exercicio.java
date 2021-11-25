db104da95ab90a17f6bac25908e1025e0b5f9193
AIRTON FRANCO DA SILVEIRA <Z215488@corp.clarobr>
2021-11-25 11:37:50 -0300

    aula arrays

--- exercicios/src/arrays/Exercicio.java
+++ exercicios/src/arrays/Exercicio.java
@@ -5,12 +5,27 @@ import java.util.Arrays;
 public class Exercicio {
 	public static void main(String[] args) {
 		double[] notasAlunoA = new double[3];
-		
+		System.out.println(Arrays.toString(notasAlunoA));
+
 		notasAlunoA[0] = 7.5;
 		notasAlunoA[1] = 9.5;
 		notasAlunoA[2] = 5.0;
 		
-		String arrays = Arrays.toString(notasAlunoA);
-		System.out.println(arrays);
+		System.out.println(Arrays.toString(notasAlunoA));
+
+		double totalAlunoA = 0;
+		for (int i = 0; i < notasAlunoA.length; i++) {
+			totalAlunoA += notasAlunoA[i];	
+		}
+
+		System.out.println(totalAlunoA / notasAlunoA.length);
+
+		double[] notasAlunoB = {6.0, 8.9, 7.2};
+		double totalAlunoB = 0;
+		for (int i = 0; i < notasAlunoB.length; i++) {
+			totalAlunoB += notasAlunoB[i];	
+		}
+
+		System.out.println(totalAlunoB / notasAlunoB.length);
 	}
 }
